c5834ced5bffd150507cddf86e7bb34af0c53c90
Satyajeet Singh <satyajeet1152000@gmail.com>
2024-12-20 00:52:55 +0530

    Image/Card Dynamic Component Done

---
+++ Scripts/data.js
@@ -0,0 +1,139 @@
+export const exploreIndiaData = [
+    {
+        imgSrc: "https://q-xx.bstatic.com/xdata/images/city/2000x2000/684534.jpg?k=d1fe86c22f2433f4e2dda14ddcbe80feb024b0fb30305e5684a1241fba5d4cff&o=",
+        properties: 2048,
+        location: "Bangalore",
+    },
+    {
+        imgSrc: "https://q-xx.bstatic.com/xdata/images/city/2000x2000/684741.jpg?k=8c41bcf34cc0d96ff2208796dc11e047e10da8adb0430b01bc2716ac99443fee&o=",
+        properties: 330,
+        location: "Mysore",
+    },
+    {
+        imgSrc: "https://r-xx.bstatic.com/xdata/images/region/2000x2000/49646.jpg?k=b7f38878b9164ee38e0b99c4d4646dbea76b7bf4add8464b1aa75e4c9d0efc6e&o=",
+        properties: 3670,
+        location: "Goa",
+    },
+    {
+        imgSrc: "https://q-xx.bstatic.com/xdata/images/city/2000x2000/684919.jpg?k=0a73fce29109503c055e288c413d9a1c5bd66fdb26f01c1438e8017b0b64b42f&o=",
+        properties: 472,
+        location: "Ooty",
+    },
+    {
+        imgSrc: "https://q-xx.bstatic.com/xdata/images/city/2000x2000/971346.jpg?k=40eeb583a755f2835f4dcb6900cdeba2a46dc9d50e64f2aa04206f5f6fce5671&o=",
+        properties: 1652,
+        location: "Mumbai",
+    },
+    {
+        imgSrc: "https://q-xx.bstatic.com/xdata/images/region/2000x2000/70669.jpg?k=8c2f6c7512748330b72821f9de3a000915e7df4dcef73711e1e4a6b68c42f38e&o=",
+        properties: 687,
+        location: "Coorg",
+    },
+    {
+        imgSrc: "https://r-xx.bstatic.com/xdata/images/city/2000x2000/822956.jpg?k=c553b16ee67434b26dae6d59a85a92169df50a1355b722ade24541356e497d9e&o=",
+        properties: 145,
+        location: "Manglore",
+    },
+    {
+        imgSrc: "https://r-xx.bstatic.com/xdata/images/city/2000x2000/684653.jpg?k=306ccafcc8a4a7e23b9e8a05b183453fe885b312a4daa5ce76ec39a1b79cbc6f&o=",
+        properties: 814,
+        location: "Hyderabad",
+    },
+    {
+        imgSrc: "https://q-xx.bstatic.com/xdata/images/city/2000x2000/684730.jpg?k=e37b93d88c1fe12e827f10c9d6909a1def7349be2c68df5de885deaa4bc01ee3&o=",
+        properties: 1406,
+        location: "Chennai",
+    },
+    {
+        imgSrc: "https://q-xx.bstatic.com/xdata/images/city/2000x2000/684769.jpg?k=146b18e42b9eb74078f2e80e07e573e8dbac879208b86bae451f99882f566a99&o=",
+        properties: 694,
+        location: "Puducherry",
+    },
+];
+
+export const browsePropertyBangloreData = [
+    {
+        imgSrc: "https://r-xx.bstatic.com/xdata/images/hotel/2000x2000/595550862.jpeg?k=3514aa4abb76a6d19df104cb307b78b841ac0676967f24f4b860d289d55d3964&o=",
+        types: "Hotels",
+        info: "20 Dec-21 Dec, 2 adults",
+        availablity: "1107",
+    },
+    {
+        imgSrc: "https://q-xx.bstatic.com/xdata/images/hotel/2000x2000/595548591.jpeg?k=01741bc3aef1a5233dd33794dda397083092c0215b153915f27ea489468e57a2&o=",
+        types: "Apartments",
+        info: "20 Dec-21 Dec, 2 adults",
+        availablity: "256",
+    },
+    {
+        imgSrc: "https://q-xx.bstatic.com/xdata/images/hotel/2000x2000/595551044.jpeg?k=262826efe8e21a0868105c01bf7113ed94de28492ee370f4225f00d1de0c6c44&o=",
+        types: "Resorts",
+        info: "20 Dec-21 Dec, 2 adults",
+        availablity: "11",
+    },
+    {
+        imgSrc: "https://q-xx.bstatic.com/xdata/images/hotel/2000x2000/620168315.jpeg?k=300d8d8059c8c5426ea81f65a30a7f93af09d377d4d8570bda1bd1f0c8f0767f&o=",
+        types: "Villas",
+        info: "20 Dec-21 Dec, 2 adults",
+        availablity: "15",
+    },
+    {
+        imgSrc: "https://r-xx.bstatic.com/xdata/images/hotel/2000x2000/595549239.jpeg?k=ad5273675c516cc1efc6cba2039877297b7ad2b5b3f54002c55ea6ebfb8bf949&o=",
+        types: "Cabins",
+        info: "20 Dec-21 Dec, 2 adults",
+        availablity: "1",
+    },
+    {
+        imgSrc: "https://q-xx.bstatic.com/xdata/images/hotel/2000x2000/595550229.jpeg?k=2ae1f5975fa1f846ac707d3334eb604a7e8f817f640cbd790185b2691532476b&o=",
+        types: "Holiday Homes",
+        info: "20 Dec-21 Dec, 2 adults",
+        availablity: "6",
+    },
+    {
+        imgSrc: "https://r-xx.bstatic.com/xdata/images/hotel/2000x2000/595550178.jpeg?k=1db9bffadd03a0f2a9f0a06ba6c7751b16465f2dd251738f229d7a57dca799ef&o=",
+        types: "Guest Houses",
+        info: "20 Dec-21 Dec, 2 adults",
+        availablity: "43",
+    },
+    {
+        imgSrc: "https://q-xx.bstatic.com/xdata/images/hotel/2000x2000/595550415.jpeg?k=8967853a074040381dfa25a568e6c780e309b529e0c144995c5bbc9644721eca&o=",
+        types: "Hostels",
+        info: "20 Dec-21 Dec, 2 adults",
+        availablity: "37",
+    },
+    {
+        imgSrc: "https://r-xx.bstatic.com/xdata/images/hotel/2000x2000/595549020.jpeg?k=f5df2d3dc0000073bef517b0cab9593036f3f1aafa2421df31a6538a8c56b834&o=",
+        types: "B&Bs",
+        info: "20 Dec-21 Dec, 2 adults",
+        availablity: "34",
+    },
+    {
+        imgSrc: "https://q-xx.bstatic.com/xdata/images/hotel/2000x2000/595550306.jpeg?k=00c1d9a10179cc21b1e7e2ad1429ac21a5e779f258cf4cf66ddce30d618c05c9&o=",
+        types: "Holiday Parks",
+        info: "20 Dec-21 Dec, 2 adults",
+        availablity: "1",
+    },
+    {
+        imgSrc: "https://r-xx.bstatic.com/xdata/images/hotel/2000x2000/595550377.jpeg?k=ef93cbc1a3af0d6db84e27b6da280a4ef24dbcfeb065fcfeae4fe9c43dddd2da&o=",
+        types: "Homestays",
+        info: "20 Dec-21 Dec, 2 adults",
+        availablity: "36",
+    },
+    {
+        imgSrc: "https://q-xx.bstatic.com/xdata/images/hotel/2000x2000/595549938.jpeg?k=88e50f4acf09b4edc03ca94818723b3baca6eeaf49bf318edf8dc6690775c480&o=",
+        types: "Campsites",
+        info: "20 Dec-21 Dec, 2 adults",
+        availablity: "1",
+    },
+    {
+        imgSrc: "https://q-xx.bstatic.com/xdata/images/hotel/2000x2000/595550059.jpeg?k=323042e47ead9072a6ca4cd3386519f9c59faff1b74043d17b486dbd5f0d5d67&o=",
+        types: "Country Houses",
+        info: "20 Dec-21 Dec, 2 adults",
+        availablity: "1",
+    },
+    {
+        imgSrc: "https://q-xx.bstatic.com/xdata/images/hotel/2000x2000/595550098.jpeg?k=d1b5a6f6faa0c76496d57dd0d263a88c07ad220b3873e14fcd71c9d8a81c5c25&o=",
+        types: "Farm Stays",
+        info: "20 Dec-21 Dec, 2 adults",
+        availablity: "5",
+    },
+];
